f7cc1255a47cc0d5419ec722e616e81f2d8c2588
mpfo0106 <mpfo0106@gmail.com>
2024-03-31 23:59:23 +0900

    :sparkles: XPG_괄호회전하기

---
+++ src/programmers/level2/XPG_괄호회전하기.java
@@ -0,0 +1,90 @@
+package programmers.level2;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+
+/**
+ * 1. O가 나올때까지 돌려. O 가 나오면 몇개 파트인지 쪼개
+ * 2. 앞에서부터 아구가 맞으면 popfirst 로 날려.
+ */
+public class XPG_괄호회전하기 {
+    public static void main(String[] args) {
+        String s = "[](){}";
+        solution(s);
+    }
+
+    public static int solution(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        int n = s.length();
+        s += s;
+        int answer = 0;
+
+        A:
+        for (int i = 0; i < n; i++) {
+            ArrayDeque<Character> stack = new ArrayDeque<>();
+            for (int j = i; j < i + n; j++) {
+                char c = s.charAt(j);
+
+                if (!map.containsKey(c)) { //여는 괄호
+                    stack.push(c);
+                } else { //닫는괄호
+                    if (stack.isEmpty() || !stack.pop().equals(map.get(c))) {
+                        continue A;
+                    }
+                }
+            }
+            if (stack.isEmpty())
+                answer += 1;
+
+        }
+        return answer;
+    }
+}
+
+//        for (int i = 0; i < s.length(); i++) {
+//            if (deque.isEmpty()) {
+//                return answer;
+//            }
+//            char ch = deque.pollFirst();
+//            if (ch == ')' || ch == '}' || ch == ']') {
+//                boolean isMatch = false;
+//                if (ch == ')') {
+//                    if (deque.peekLast() == '(') {
+//                        isMatch = true;
+//                    }
+//                }
+//                if (ch == '}') {
+//                    if (deque.peekLast() == '{') {
+//                        isMatch = true;
+//                    }
+//                }
+//                if (ch == ']') {
+//                    if (deque.peekLast() == '[') {
+//                        isMatch = true;
+//                    }
+//                }
+//                if (isMatch) {
+//                    deque.pollLast();
+//                    answer += 1;
+//                }
+//            } else {
+//                deque.addLast(ch);
+//            }
+//
+//        }
+//        return answer;
+//
+//    }
+//}
+/**
+ * 1. deque 을 이용해서 첫번째와 마지막이 만나면 pop을 하려했음 => 일부 정답이지만 nullpointer 에러
+ * switch case 문을 이용하기.
+ * <p>
+ * <p>
+ * 1. hashMap 으로 저장을 다 해두고 containsKey 를 사용해서 코드 양을 줄이기.
+ * 2. A: 같은 label 문법의 사용
+ */
