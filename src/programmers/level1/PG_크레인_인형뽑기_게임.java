2ad8c10f0874a8acd9a6ba085553969414bb32ef
mpfo0106 <mpfo0106@gmail.com>
2024-04-01 21:13:34 +0900

    :sparkles: PG_크레인_인형뽑기_게임_Stack_사용

--- src/programmers/level1/PG_크레인_인형뽑기_게임.java
+++ src/programmers/level1/PG_크레인_인형뽑기_게임.java
@@ -18,29 +18,61 @@ public class PG_크레인_인형뽑기_게임 {
 
     }
 
-    public static int solution(int[][] board, int[] moves) {
-        Stack<Integer> stack = new Stack<>();
+    public int solution(int[][] board, int[] moves) {
+        Stack<Integer>[] lanes = new Stack[board.length];
+        for (int i = 0; i < lanes.length; i++) {
+            lanes[i] = new Stack<>();
+        }
+        for (int i = board.length - 1; i >= 0; i--) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] > 0)
+                    lanes[j].push(board[i][j]);
+            }
+        }
+        Stack<Integer> bucket = new Stack<>();
         int answer = 0;
         for (int move : moves) {
-            for (int i = 0; i < board.length; i++) {
-                int num = board[i][move - 1];
-                if (num != 0) {
-                    if (!stack.isEmpty() && stack.peek() == num) {
-                        stack.pop();
-                        answer += 2;
-                    } else {
-                        stack.push(num);
-                    }
-                    board[i][move - 1] = 0; //뽑으면 0으로 변경
-                    break;
+            if (!lanes[move - 1].isEmpty()) {
+                int doll = lanes[move - 1].pop();
+                if (!bucket.isEmpty() && bucket.peek() == doll) {
+                    bucket.pop();
+                    answer += 2;
+                } else {
+                    bucket.push(doll);
                 }
             }
+
         }
         return answer;
     }
+
+//    public static int solution(int[][] board, int[] moves) {
+//        Stack<Integer> stack = new Stack<>();
+//        int answer = 0;
+//        for (int move : moves) {
+//            for (int i = 0; i < board.length; i++) {
+//                int num = board[i][move - 1];
+//                if (num != 0) {
+//                    if (!stack.isEmpty() && stack.peek() == num) {
+//                        stack.pop();
+//                        answer += 2;
+//                    } else {
+//                        stack.push(num);
+//                    }
+//                    board[i][move - 1] = 0; //뽑으면 0으로 변경
+//                    break;
+//                }
+//            }
+//        }
+//        return answer;
+//    }
+
 
 }
 /**
  * 1.뽑기해서 pop 되면 +2 안되면 push 로 구현 =>  테케까지는 통과..
  * 2차시도. break 문의 위치 실수, 뽑으면 0으로 바꿔야하는걸 까먹었다! => 해결!
+ * <p>
+ * ---
+ * 정답 풀이는 Stack 배열 사용
  */
