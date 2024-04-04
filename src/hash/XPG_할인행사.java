3120948ad1c317c39afef6577c186d323de5ed1f
mpfo0106 <mpfo0106@gmail.com>
2024-04-04 16:17:45 +0900

    :sparkles: XPG_할인행사

---
+++ src/hash/XPG_할인행사.java
@@ -0,0 +1,41 @@
+package hash;
+
+import java.util.HashMap;
+
+/**
+ * HashMap 에 want 저장해두고
+ * discount 를 돌면서 연속으로 되는지 확인
+ * <p>
+ * O(N + M) 이라 가능할것같음
+ */
+public class XPG_할인행사 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(String[] want, int[] number, String[] discount) {
+        HashMap<String, Integer> wantMap = new HashMap<>();
+
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+        int answer = 0;
+        for (int i = 0; i < discount.length - 9; i++) {
+            HashMap<String, Integer> discount10 = new HashMap<>();
+            for (int j = i; j < i + 10; j++) {
+                if (wantMap.containsKey(discount[j])) {
+                    discount10.put(discount[j], discount10.getOrDefault(discount[j], 0) + 1);
+                }
+            }
+            if (wantMap.equals(discount10)) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
+/**
+ * HashMap 으로 접근 하는건 알았지만, 효율적 접근법이 생각나지 않았음.
+ * <p>
+ * map 의 equals 를 사용하자
+ */
