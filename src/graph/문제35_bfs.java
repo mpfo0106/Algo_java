98d61b0b43ed3dc2baa773e50cc93923c7a26e37
mpfo0106 <mpfo0106@gmail.com>
2024-05-01 21:52:15 +0900

    :sparkles: 문제35_bfs

--- src/graph/문제35_bfs.java
+++ src/graph/문제35_bfs.java
@@ -4,45 +4,47 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class 문제35_bfs {
+    private static ArrayList<Integer> answer;
     private static ArrayList<Integer>[] adjList;
     private static boolean[] visited;
-    private static ArrayList<Integer> answer;
 
-    private static void bfs(int start) {
+    public static void main(String[] args) {
+        int[][] graph = {{1, 3}, {3, 4}, {3, 5}, {5, 2}};
+        int start = 1;
+        int n = 5;
+        solution(graph, start, n);
+        System.out.println(answer);
+    }
+
+    private static void bfs(int now) {
         ArrayDeque<Integer> queue = new ArrayDeque<>();
-        visited[start] = true;
-        queue.addLast(start);
+        queue.add(now);
+        visited[now] = true;
         while (!queue.isEmpty()) {
-            int now = queue.pollFirst();
-            answer.add(now); // 한번으로 가능
-            for (int next : adjList[now]) {
+            int num = queue.poll();
+            answer.add(num);
+            
+            for (int next : adjList[num]) {
                 if (!visited[next]) {
-                    queue.addLast(next);
+                    queue.add(next);
                     visited[next] = true;
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
-
-
-    }
-
     private static int[] solution(int[][] graph, int start, int n) {
         adjList = new ArrayList[n + 1];
-        for (int i = 0; i < adjList.length; ) {
+        visited = new boolean[n + 1];
+        answer = new ArrayList<>();
+        for (int i = 0; i < adjList.length; i++) {
             adjList[i] = new ArrayList<>();
         }
         for (int[] edge : graph) {
             adjList[edge[0]].add(edge[1]);
         }
-        visited = new boolean[n + 1];
-        answer = new ArrayList<>();
         bfs(start);
-
         return answer.stream().mapToInt(Integer::intValue).toArray();
-
     }
 }
 
