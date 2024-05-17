614e6d46259ce3cbce8781e34741490888447a6f
강준호 <mpfo0106@gmail.com>
2024-05-18 00:21:07 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1104 ms, Memory: 189060 KB -BaekjoonHub

--- Java/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ Java/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -11,27 +11,17 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         String[] str1 = br.readLine().split(" ");
-        int[] cards = new int[N];
-        for (int i = 0; i < cards.length; i++) {
-            cards[i] = Integer.parseInt(str1[i]);
+        for (int i = 0; i < N; i++) {
+            int card = Integer.parseInt(str1[i]);
+            hashMap.put(card, hashMap.getOrDefault(card, 0) + 1);
         }
         int M = Integer.parseInt(br.readLine());
         String[] str2 = br.readLine().split(" ");
-        int[] myCard = new int[M];
-        for (int i = 0; i < myCard.length; i++) {
-            myCard[i] = Integer.parseInt(str2[i]);
+
+        for (int i = 0; i < M; i++) {
+            int card = Integer.parseInt(str2[i]);
+            sb.append(hashMap.getOrDefault(card, 0)).append(" ");
         }
-        for (int card : myCard) {
-            hashMap.put(card, 0);
-        }
-        for (int card : cards) {
-            if (hashMap.containsKey(card)) {
-                hashMap.put(card, hashMap.getOrDefault(card, 0) + 1);
-            }
-        }
-        for (int card : myCard) {
-            sb.append(hashMap.get(card)).append(" ");
-        }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
